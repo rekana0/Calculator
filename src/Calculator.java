1b5ecf8a6ea86fc2e68fc8b9c36c6ef994d668d5
rekana0 <43593496+rekana0@users.noreply.github.com>
2019-01-19 12:20:15 -0700

    Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,4 +7,7 @@ public class Calculator {
     public static double subtract(double num1, double num2){
         return (num1-num2);
     }
+    public static doble multiblication(double num1, double num2){
+     return (num1*num2);   
+    }
 }
